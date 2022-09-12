1ee1a505ca62e7180b62a588bb90080a5b9c8204
Korwenaktha <106329290+Korwenaktha@users.noreply.github.com>
2022-09-12 21:37:50 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 // MENU ICON / BOUTON 
 let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
 menu.onclick = () => {
+    navbar.classList.toggle("open-menu")
     menu.classList.toggle("move");
+};
+window.onscroll = () => {
+    navbar.classList.remove("open-menu")
+    menu.classList.remove("move");
 }
 
 // HEADER BACKGROUND CHANGE ON SCROLL
